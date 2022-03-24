d8345fa69fceba23fd188d7b93e8552b22f3d9da
Riana Santatra RAZAFIMAMONJY - Charbon <rianasantatra665@gmail.com>
2022-03-24 13:40:18 +0300

    update files

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -21,8 +21,8 @@ class Register extends Controller
         helper(['form']);
         //set rules validation form
         $rules = [
-            'name'          => 'required|min_length[3]|max_length[20]',
-            'email'         => 'required|min_length[6]|max_length[50]|valid_email|is_unique[users.user_email]',
+            'im'          => 'required|min_length[3]|max_length[20]',
+            'username'         => 'required|min_length[6]|max_length[50]|valid_email|is_unique[users.user_email]',
             'password'      => 'required|min_length[6]|max_length[200]',
             'confpassword'  => 'matches[password]'
         ];
@@ -30,9 +30,9 @@ class Register extends Controller
         if ($this->validate($rules)) {
             $model = new UserModel();
             $data = [
-                'user_name'     => $this->request->getVar('name'),
-                'user_email'    => $this->request->getVar('email'),
-                'user_password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+                'im'     => $this->request->getVar('im'),
+                'username'    => $this->request->getVar('username'),
+                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
             ];
             $model->save($data);
             return redirect()->to('/login');
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,13 +7,5 @@ use CodeIgniter\Model;
 class UserModel extends Model
 {
     protected $table = 'users';
-    protected $allowedFields = ['im', 'username', 'password'];
-
-    protected function passwordHash(array $data)
-    {
-        if (isset($data['data']['password']))
-            $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
-
-        return $data;
-    }
+    protected $allowedFields = ['im', 'username', 'password', 'created_at'];
 }
