595d05642cf0270d9ad85115c75e8b89064afedd
Riana Santatra RAZAFIMAMONJY - Charbon <rianasantatra665@gmail.com>
2022-03-26 13:23:59 +0300

    updating base_url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://demo/';
+    public $baseURL = 'http://gestion-hopital/';
 
     /**
      * --------------------------------------------------------------------------
